ae1e3d4684879978d4a42170db352208f55538cd
DanAletor <danaletordaniela@gmail.com>
2022-09-13 12:51:09 -0700

    l

---
+++ 0x02-functions_nested_loops/_putchar.h
@@ -0,0 +1,69 @@
+#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char *sh = "_putchar";
+
+        while (*sh)
+        {
+                _putchar(*sh);
+                sh++;
+        }
+        _putchar('\n');
+
+        return (0);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"0-putchar.c" 19L, 204C                                                 1,20          All
+#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char *sh = "_putchar";
+
+        while (*sh)
+        {
+                _putchar(*sh);
+                sh++;
+        }
+        _putchar('\n');
+
+        return(0);
+}
+ 
